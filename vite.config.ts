fdc1ae81e8882908644fe78ea805040645cc83bd
Sukreet018 <sukreet018@gmail.com>
2026-09-10 00:18:22 +0530

    Move contact section to separate page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/gymx/",
   plugins: [react(), tailwindcss()],
 });
